d13bd4fea04eddb95ab2df53d653b60dd69f4966
DESKTOP-7KB13SA\david <davidroddriguez896@gmail.com>
2024-02-13 15:28:57 -0600

    Navbar terminado version desktop y diseño del footer en curso.

--- codeminds-web/src/app/app-routing.module.ts
+++ codeminds-web/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { MainLayout } from '@shared/layout/main/main.layout';
+const routes: Routes = [{
+  path:'',
+  component: MainLayout 
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ codeminds-web/src/app/modules/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainLayout } from './layout/main/main.layout';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [
+    MainLayout
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class SharedModule { }
