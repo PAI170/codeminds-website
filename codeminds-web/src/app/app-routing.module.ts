e2ecf63bafc788a584976951e4517a6de9c2eabe
DESKTOP-7KB13SA\david <davidroddriguez896@gmail.com>
2024-02-14 15:07:47 -0600

    Diseño base del home finalizado, pendiente diseño con CSS.

--- codeminds-web/src/app/app-routing.module.ts
+++ codeminds-web/src/app/app-routing.module.ts
@@ -3,7 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainLayout } from '@shared/layout/main/main.layout';
 const routes: Routes = [{
   path:'',
-  component: MainLayout 
+  component: MainLayout,
+  children: [
+    {
+      path:'',
+      loadChildren: () => import('@home/home.module').then((m) => m.HomeModule)
+    }
+  ] 
 }];
 
 @NgModule({
---
+++ codeminds-web/src/app/modules/home/home.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePage } from './pages/home/home.page';
+import { SharedModule } from '@shared/shared.module';
+import { HomeRoutingModule } from './home-routing.module';
+import { CursosComponent } from './pages/cursos/cursos.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomePage,
+    CursosComponent
+  ],
+  imports: [
+    SharedModule,
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
